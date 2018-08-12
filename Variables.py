61d51f3b8fdf0d8d141dde4ff9e4d0cfc3de6fb2
JulsD <dutchak.julia@gmail.com>
2018-08-12 21:35:01 +0200

    variables

---
+++ Variables.py
@@ -0,0 +1,22 @@
+a = "Hello"
+b = 25
+
+print(a)
+print(b)
+
+f_name = "Homer"
+l_name = "Simpson"
+age = 45
+full_name = f_name + " " + l_name
+print(full_name + " " + str(age))
+
+case_sensetive = "lowercase"
+CASE_SENSETIVE = "UPPERCASE"
+Case_Sensetive = "Capitalize"
+print(case_sensetive + " - " + CASE_SENSETIVE + " - " + Case_Sensetive)
+
+num1 = 11111
+num2 = 33333
+num3 = num1 + num2
+print(num3)
+print(num1 + num2)
