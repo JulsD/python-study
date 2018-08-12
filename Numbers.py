0130548fbe93c242dabad893959dc7f75d95d727
JulsD <dutchak.julia@gmail.com>
2018-08-12 22:04:31 +0200

    numbers

---
+++ Numbers.py
@@ -0,0 +1,16 @@
+num1 = 30
+num2 = 5
+num3 = num1 + 10
+print(num1 + num2)
+print(num3)
+
+x1 = 11111111111111111111111111111111111
+x2 = 222222222222222222222
+print(x1 * x2 + 10)
+print(x1 / x2 + num1)
+
+x1 = 6
+x2 = 4
+
+print(x1/x2)
+print(1+3/(6*2))
