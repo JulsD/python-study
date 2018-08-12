94cdd6e0329ea7a8309d14bc853b62455fbd942d
JulsD <dutchak.julia@gmail.com>
2018-08-12 22:31:04 +0200

    loops

---
+++ Loops.py
@@ -0,0 +1,12 @@
+for x in range(20, -10, -2):
+    print("***" + str(x) + "***")
+    if x == 0:
+        break
+
+print("-----END-----")
+
+while True:
+    print(x)
+    x+=1
+    if x == 10:
+        break
