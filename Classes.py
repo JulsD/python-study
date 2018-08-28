75e99497d6c5e4d5b9c6c3f6951031e485a54bf8
JulsD <dutchak.julia@gmail.com>
2018-08-28 23:30:20 +0200

    add class inheritance

--- Classes.py
+++ Classes.py
@@ -1,7 +1,8 @@
-from heroClass import HeroOf
+from heroClass import *
 
 myHero1 = HeroOf("Frodo", 20, "Elf")
 myHero2 = HeroOf("Ron", 17, "wizard")
+mySuperHero1 = SuperHeroOf("Ironman", 17, "humn", "Marvel")
 
 myHero1.show_hero()
 myHero1.move_hero()
@@ -12,3 +13,10 @@ myHero2.show_hero()
 myHero2.level_up()
 myHero2.set_health(50)
 myHero2.show_hero()
+
+mySuperHero1.show_hero()
+mySuperHero1.use_armo()
+mySuperHero1.use_armo()
+mySuperHero1.__armo = 11
+mySuperHero1.use_armo()
+mySuperHero1.show_hero()
--- heroClass.py
+++ heroClass.py
@@ -19,3 +19,21 @@ class HeroOf():
     def move_hero(self):
         """Start moving hero"""
         print("Hero " + self.name + " started move")
+
+class SuperHeroOf(HeroOf):
+    """class for super hero"""
+    def __init__(self, name, level, race, universe):
+        """init our super hero"""
+        super().__init__(name, level, race)
+        self.universe = universe
+        self.__armo = 100
+    def use_armo(self):
+        """using armo and decrease it by one point"""
+        print(str(self.name) + " is using armo")
+        self.__armo-= 1
+        print(str(self.__armo) + " lefted")
+    def show_hero(self):
+        """Prints params of the hero"""
+        descr = (self.name.title() + " of " + str(self.level) + " level with " + self.race + 
+                "race has " + str(self.health) + " health and belongs to " + self.universe.title() + " universe")
+        print(descr)
