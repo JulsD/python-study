c24e5ff1fbc17a96786e6baa8694617152ecd34f
JulsD <dutchak.julia@gmail.com>
2018-08-28 23:13:43 +0200

    separate hero class in a separate file

--- Classes.py
+++ Classes.py
@@ -1,24 +1,4 @@
-class HeroOf():
-    """class for hero of the game"""
-    def __init__(self, name, level, race):
-        """init our hero"""
-        self.name = name
-        self.level = level
-        self.race = race
-        self.health = 100
-    def show_hero(self):
-        """Prints params of the hero"""
-        descr = (self.name.title() + " of " + str(self.level) + " level with " + self.race + "race has " + str(self.health) + " health")
-        print(descr)
-    def level_up(self):
-        """Rise level of the hero on one point"""
-        self.level += 1
-    def set_health(self, new_health):
-        """Set new health to the hero"""
-        self.health = new_health
-    def move_hero(self):
-        """Start moving hero"""
-        print("Hero " + self.name + " started move")
+from heroClass import HeroOf
 
 myHero1 = HeroOf("Frodo", 20, "Elf")
 myHero2 = HeroOf("Ron", 17, "wizard")
